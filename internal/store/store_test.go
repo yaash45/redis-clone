747cbb628ec43a96eacb487a4bb2046c5fe75b83
Yaash Jain <yaash.jain45@gmail.com>
2026-03-29 15:17:45 -0700

    Improve initial CLI and fix tests

--- internal/store/store_test.go
+++ internal/store/store_test.go
@@ -7,9 +7,9 @@ import (
 func TestEmptyStoreAccess(t *testing.T) {
 	kvs := NewStore()
 
-	val := kvs.Get("foo")
+	_, err := kvs.Get("foo")
 
-	if val != "" {
+	if err == nil {
 		t.Error("Store is not empty.")
 	}
 }
@@ -18,28 +18,32 @@ func TestBasicApi(t *testing.T) {
 	kvs := NewStore()
 
 	kvs.Set("foo", "bar")
+	val, err := kvs.Get("foo")
 
-	if kvs.Get("foo") != "bar" {
+	if err != nil && val != "bar" {
 		t.Fail()
 	}
 
 	kvs.Set("hi", "hello")
+	val, err = kvs.Get("hi")
 
-	if kvs.Get("hi") != "hello" {
+	if err != nil && val != "hello" {
 		t.Fail()
 	}
 
 	// Overwrite a key's value
 	kvs.Set("hi", "bye")
+	val, err = kvs.Get("hi")
 
-	if kvs.Get("hi") != "bye" {
+	if err != nil && val != "bye" {
 		t.Fail()
 	}
 
 	// Delete a key
 	kvs.Delete("foo")
+	_, err = kvs.Get("foo")
 
-	if kvs.Get("foo") != "" {
+	if err == nil {
 		t.Fail()
 	}
 }
--- redis.go
+++ redis.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	fmt.Println("Redis clone starting...")
-	fmt.Printf("Ready. Enter commands:\n\n")
+	fmt.Printf("Ready. Enter commands in format [SET/GET/DEL] [KEY] [VALUE]. Enter 'exit' to stop:\n\n")
 
 	kvs := store.NewStore()
 
@@ -18,6 +18,7 @@ func main() {
 		var key string
 		var value string
 
+		fmt.Printf("> ")
 		fmt.Scanln(&cmd, &key, &value)
 
 		if cmd == "GET" {
@@ -25,29 +26,33 @@ func main() {
 			lookup_result, err := kvs.Get(key)
 
 			if err != nil {
-				fmt.Printf("> [miss] key '%s' does not exist\n\n", key)
+				fmt.Printf("key '%s' does not exist", key)
 			} else {
-				fmt.Printf("> %s\n\n", lookup_result)
+				fmt.Printf("%s", lookup_result)
 			}
 
 		} else if cmd == "SET" {
 
-			kvs.Set(key, value)
-			fmt.Printf(">\n\n")
+			if key == "" || value == "" {
+				fmt.Printf("[error] 'SET' command requires non-empty key and value.")
+
+			} else {
+				kvs.Set(key, value)
+				fmt.Printf("[ok]")
+			}
 
 		} else if cmd == "DEL" {
-
 			kvs.Delete(key)
-			fmt.Printf(">\n\n")
+			fmt.Printf("[ok]")
 
 		} else if strings.ToLower(cmd) == "exit" {
-
 			break
 
 		} else {
-
-			fmt.Printf("Unrecognized command, try again.\n\n")
+			fmt.Printf("Unrecognized command, try again.")
 
 		}
+
+		fmt.Printf("\n\n")
 	}
 }

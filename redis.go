70626734c5f77ef927c4ded19168c07300665b5f
Yaash Jain <yaash.jain45@gmail.com>
2026-03-28 17:15:47 -0700

    Simple CLI loop for GET, SET and DEL

--- redis.go
+++ redis.go
@@ -2,8 +2,36 @@ package main
 
 import (
 	"fmt"
+	"strings"
+
+	store "github.com/yaash45/redis/internal/store"
 )
 
 func main() {
 	fmt.Println("Redis clone starting...")
+	fmt.Printf("Ready. Enter commands:\n\n")
+
+	kvs := store.NewStore()
+
+	for {
+		var cmd string
+		var key string
+		var value string
+
+		fmt.Scanln(&cmd, &key, &value)
+
+		if cmd == "GET" {
+			fmt.Printf("> %s\n\n", kvs.Get(key))
+		} else if cmd == "SET" {
+			kvs.Set(key, value)
+			fmt.Printf(">\n\n")
+		} else if cmd == "DEL" {
+			kvs.Delete(key)
+			fmt.Printf(">\n\n")
+		} else if strings.ToLower(cmd) == "exit" {
+			break
+		} else {
+			fmt.Printf("Unrecognized command, try again.\n\n")
+		}
+	}
 }
